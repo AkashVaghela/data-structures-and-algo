ca8ff9971cc7778671fcbe429faf24373af3f950
akash vaghela <akashvaghela.dev@gmail.com>
2023-03-11 14:52:03 +0530

    completed dynamic programming

--- 12-dynamic-programming.js
+++ 12-dynamic-programming.js
@@ -1,1 +1,62 @@
 // dynamic programming
+
+// we are storing cache in global namespace which is not a good practice. so we can use closures to optimize it without polluting a global namespace.
+let cache = {};
+
+const memoizedAddTo69 = (n) => {
+  if (n in cache) {
+    return cache[n];
+  } else {
+    console.log('Long calculation');
+    return (cache[n] = n + 69);
+  }
+};
+
+console.log(memoizedAddTo69(1));
+console.log(memoizedAddTo69(1));
+
+const memoizedAddTo69WithClosures = () => {
+  let cache = {};
+
+  return (n) => {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      console.log('Long calculation');
+      return (cache[n] = n + 69);
+    }
+  };
+};
+
+let memoized = memoizedAddTo69WithClosures();
+
+console.log(memoized(1));
+console.log(memoized(1));
+
+const fibonacci = (n) => {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+const memoizedFibonacci = () => {
+  let cache = {};
+  return function memoizedFib(n) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      if (n < 2) {
+        return n;
+      } else {
+        cache[n] = memoizedFib(n - 1) + memoizedFib(n - 2);
+        return cache[n];
+      }
+    }
+  };
+};
+
+const memoizedFib = memoizedFibonacci();
+
+console.log(memoizedFib(1));
+console.log(memoizedFib(1));
--- 12-dynamic-programming.md
+++ 12-dynamic-programming.md
@@ -1,1 +1,19 @@
 # dynamic programming
+
+Dynamic Programming is mainly an optimization over plain recursion. Wherever we see a recursive solution that has repeated calls for same inputs, we can optimize it using Dynamic Programming. The idea is to simply store the results of subproblems, so that we do not have to re-compute them when needed later.
+
+# cache
+
+A cache is simply a temporary data store that holds data so that future requests for that data can be served faster.
+
+# memoization
+
+- it is one type of caching
+- it is a form of caching which involves caching the return value of function based on it's parameters
+
+# when to apply DP?
+
+1. a problem can be divided into subproblems
+2. recursive solution
+3. are there repetitive subproblems?
+4. memoize subproblems
